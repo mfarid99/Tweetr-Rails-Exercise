84215d222cae62c2cb8fc1b3c349e139088d0ad6
moe <moe.faridd@gmail.com>
2020-11-23 19:49:55 -0500

    commit4: Created model and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Tweet.create ({
+    title: "Politicians Today!!",
+    content: "The politicians and the corporate hyenas cannot be told apart",
+    author: "mofassa"
+})
+
+Tweet.create ({
+    title: "Mandatory Masks",
+    content: "The administration must mandate wearing masks throughout all 50 states",
+    author: "Krystall Ball"
+})

242928752f96db2c25e4fb05dea9c3d82235672f
moe <moe.faridd@gmail.com>
2020-11-23 21:01:24 -0500

    Commit 8: Can Post new Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,7 +7,21 @@ class TweetsController < ApplicationController
 
     def show
         tweet = Tweet.find(params[:id])
-        render(json: {tweet: tweet})
+        render(json: {status: 200, tweet: tweet})
+    end
+
+    def create
+        tweet = Tweet.new(tweet_params)
+        if tweet.save
+            render json: {tweet: tweet, status: 201}
+        else
+            render json: {tweet: tweet, errors: song.errors}
+    end
+end
+    private
+
+    def tweet_params
+        params.required(:tweet).permit(:title, :content, :author)
     end
 
 end
